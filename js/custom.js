a906eadb1f0bb852f64785767c85d58ae3e2f46e
George Thomas <tgeorgetom@gmail.com>
2018-11-04 16:45:26 +0530

    Home-page

---
+++ js/custom.js
@@ -0,0 +1,3 @@
+var colors = ['#C9DDE5','#F9CB53', '#F4B266', '#CB8589', '#48B8D0', '#82968C'];
+
+document.getElementById('hero-banner-content').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
