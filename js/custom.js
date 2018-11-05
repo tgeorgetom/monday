67170f4d9635c6a9a252bd9f55adc5ad208d3fa6
George Thomas <tgeorgetom@gmail.com>
2018-11-05 17:00:06 +0530

    parallax

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,38 @@
+// Dynamic background
+
 var colors = ['#C9DDE5','#F9CB53', '#F4B266', '#CB8589', '#48B8D0', '#82968C'];
 
 document.getElementById('hero-banner-content').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+
+// Parallax Init
+
+var parallax = new Parallax('.parallax', {})
+parallax.on('image:loaded', function() {
+  console.log(arguments)
+})
+parallax.on('images:loaded', function() {
+  console.log(arguments)
+})
+parallax.init();
+
+
+// Scroll section
+
+
+$(document).ready(function(){
+  $(".hero-down-arrow").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000,  function(){
+   
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
