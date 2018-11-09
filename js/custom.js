4b4b6aef64e8c3b81bfd59d6110f8be58a87992e
George Thomas <tgeorgetom@gmail.com>
2018-11-09 08:40:44 +0530

    Updates

--- js/custom.js
+++ js/custom.js
@@ -111,3 +111,35 @@ $("#sayHello").validate({
     $(form).submit();
   }
  });
+
+
+
+$(document).ready(function () {
+    // Read the cookie and if it's defined scroll to id
+    var scroll = $.cookie('scroll');
+    if(scroll){
+        scrollToID(scroll, 1000);
+        $.removeCookie('scroll');
+    }
+
+    // Handle event onclick, setting the cookie when the href != #
+    $('.nav a').click(function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var href = $(this).attr('href');
+        if(href === '#'){
+            scrollToID(id, 1000);
+        }else{
+            $.cookie('scroll', id);
+            window.location.href = href;
+        }
+    });
+
+    // scrollToID function
+    function scrollToID(id, speed) {
+        var offSet = 70;
+        var obj = $('#' + id).offset();
+        var targetOffset = obj.top - offSet;
+        $('html,body').animate({ scrollTop: targetOffset }, speed);
+    }
+});
